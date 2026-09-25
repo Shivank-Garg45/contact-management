572a307cdeff70dbca90da6eb7f7bcb4a68523c5
Shivank <shivankgarg664@gmail.com>
2026-09-25 20:16:58 +0000

    Update groups header

--- groups.h
+++ groups.h
@@ -1,18 +1,27 @@
 #ifndef GROUPS_H
 #define GROUPS_H
 
-#include "contact.h"
+#define MAX_GROUPS 50
+#define MAX_GROUP_NAME_LEN 32
+#define MAX_GROUP_DESC_LEN 128
 
 typedef struct {
-    char GroupName[20];
-    int memberCount;
-    Contact members[3];
+    int id;
+    char name[MAX_GROUP_NAME_LEN];
+    char description[MAX_GROUP_DESC_LEN];
 } Group;
 
-extern Group groups[10];
+extern Group groups[MAX_GROUPS];
 extern int groupCount;
 
+void loadGroups(void);
+void saveGroups(void);
+void clearGroups(void);
 void createGroup(void);
 void showGroups(void);
+void deleteGroup(void);
+void manageGroupsMenu(void);
+int groupExists(const char *name);
+int getContactCountInGroup(const char *groupName);
 
-#endif
+#endif // GROUPS_H
